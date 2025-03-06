b17fdc7fc78c2511a192052290e0a9241bd64dd2
dev5576 <pronay@enzan-k.com>
2025-03-06 16:34:05 +0900

    predict diabetes using random forest

--- hello.py
+++ hello.py
@@ -14,7 +14,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 #from sklearn.feature_selection import RFE
-#from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import plot_tree
 
 
 
@@ -39,6 +40,7 @@ def plot_iris():
     plt.show()
 
 def predictIrisFlower():
+    print("\n")
     iris = datasets.load_iris()
     #print(iris.data.shape)
     X = iris.data
@@ -65,6 +67,7 @@ def predictIrisFlower():
     plot_iris()
     
 def detectBreastCancer():
+    print("\n")
     breast_cancer = datasets.load_breast_cancer()
     X = breast_cancer.data
     y= breast_cancer.target
@@ -76,10 +79,51 @@ def detectBreastCancer():
     print(f"BreastCancer detection Accuracy: {accuracy_score(y_test, y_pred)}")
 
 
+
+def predictDiabetes():
+    print("\n")
+    df = pd.read_csv('diabetes.csv')
+    #print(df.head())
+
+    X = df.drop('Outcome',axis=1)
+    y = df['Outcome']
     
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.7, random_state=42)
+    classifier_rf = RandomForestClassifier(random_state=42, n_jobs=-1, max_depth=5,
+                                       n_estimators=100, oob_score=True)
+    classifier_rf.fit(X_train, y_train)
+    #print(classifier_rf.oob_score_)
+
+    rf = RandomForestClassifier(random_state=42, n_jobs=-1)
+
+    params = {
+        'max_depth': [2,3,5,10,20],
+        'min_samples_leaf': [5,10,20,50,100,200],
+        'n_estimators': [10,25,30,50,100,200]
+    }
+
+
+    # Instantiate the grid search model
+    grid_search = GridSearchCV(estimator=rf,
+                            param_grid=params,
+                            cv = 4,
+                            n_jobs=-1, verbose=1, scoring="accuracy")
+
+  
+    grid_search.fit(X_train, y_train)
+    
+    y_pred = classifier_rf.predict(X_test)  
+    print(f"detection Accuracy of Diabetics patient: {accuracy_score(y_test, y_pred)}")
+    
+    #print(grid_search.best_score_)
+    rf_best = grid_search.best_estimator_
+    #print(rf_best)
+ 
+
     
 if __name__ == "__main__":
     detectBreastCancer()
+    predictDiabetes()
     predictIrisFlower()
    
     #main()
