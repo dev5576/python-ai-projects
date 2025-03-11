2a8af2d75675c30280d82dc3d014b7943d4b127f
dev5576 <pronay@enzan-k.com>
2025-03-11 17:01:03 +0900

    added fucntion for log odd

--- hello.py
+++ hello.py
@@ -181,12 +181,17 @@ def hierarchicalCluster():
 
 
 #reference: https://www.w3schools.com/python/python_ml_logistic_regression.asp
+def logit2prob(logr, x):
+    log_odds = logr.intercept_ + logr.coef_[0] * x
+    odds = np.exp(log_odds) 
+    prob = odds / (1 + odds)
+    return prob
+
+
+
 def logisticRegression():
-    #X represents the size of a tumor in centimeters.
     X = np.array([3.78, 2.44, 2.09, 0.14, 1.72, 1.65, 4.92, 4.37, 4.96, 4.52, 3.69, 5.88]).reshape(-1,1)
 
-    #Note: X has to be reshaped into a column from a row for the LogisticRegression() function to work.
-    #y represents whether or not the tumor is cancerous (0 for "No", 1 for "Yes").
     y = np.array([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1])
 
     logr = linear_model.LogisticRegression()
@@ -194,6 +199,13 @@ def logisticRegression():
 
     predicted = logr.predict(np.array([3.46]).reshape(-1,1))
     print(predicted)
+
+    log_odds = logr.coef_
+    odds = np.exp(log_odds)
+    print(odds)
+
+
+    print(logit2prob(logr, X))
     
     
 if __name__ == "__main__":
